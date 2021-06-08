7f2956d2a4ebadc2808dea56d6d0d62c64680bc7
abondar24 <abondar1992@gmail.com>
2021-06-08 12:41:49 +0200

    nlp module main

--- NaturalLangProcessing/src/main/java/org/abondar/experimental/ml4j/nlp/Main.java
+++ NaturalLangProcessing/src/main/java/org/abondar/experimental/ml4j/nlp/Main.java
@@ -1,7 +1,18 @@
 package org.abondar.experimental.ml4j.nlp;
 
+import org.abondar.experimental.ml4j.command.CommandSwitcher;
+import org.abondar.experimental.ml4j.nlp.command.NlpCommandSwitcher;
+import org.bytedeco.javacpp.tools.Slf4jLogger;
+
 public class Main {
     public static void main(String[] args) {
-
+        var logger = new Slf4jLogger(Main.class);
+        CommandSwitcher ncs = new NlpCommandSwitcher();
+        if (args.length==0){
+            logger.error("Missing argument. Please check documentation for available arguments");
+            System.exit(0);
+        }
+        String cmd = args[0].toUpperCase();
+        ncs.executeCommand(cmd);
     }
 }
