11e6a28afac6605012089026f47d0d2f1adc93c5
abondar24 <abondar1992@gmail.com>
2021-06-18 19:53:00 +0200

    removed rl4j

--- ReinforcementLearning/src/main/java/org/abondar/experimental/ml4j/rl4j/Main.java
+++
@@ -1,8 +0,0 @@
-package org.abondar.experimental.ml4j.rl4j;
-
-public class Main {
-    public static void main(String[] args) {
-      var ml = new MalmoNet();
-      ml.performComputations();
-    }
-}
